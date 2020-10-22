b7c0407077218cd9293eeacfbe1d62e261335e39
[maxbek] <[maxbek294@gmail.com]>
2020-10-22 18:33:50 +0300

    Add ship

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings():
+    """Класс для хранения всех настроек игры Alien Invasion."""
+    def __init__(self):
+        # Параметры экрана
+        self.screen_width = 1600
+        self.screen_height = 900
+        self.bg_color = (4, 8, 35)
